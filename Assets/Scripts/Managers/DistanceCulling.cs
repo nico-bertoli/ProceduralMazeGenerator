2c77916613e35b31fb1ac0ef6ef6d9f805fe352e
nico-bertoli <nicobertoli.1999@gmail.com>
2024-06-11 07:33:48 +0200

    Distance culling refactored

--- Assets/Scripts/Managers/DistanceCulling.cs
+++ Assets/Scripts/Managers/DistanceCulling.cs
@@ -1,63 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Activates the object when a certain target is close
-/// </summary>
+
 public class DistanceCulling : MonoBehaviour
 {
-    //======================================== fields
-    /// <summary>
-    /// How often the coroutine checks if player is near (seconds)
-    /// </summary>
-    private static float checkRateo = 1f;
-    /// <summary>
-    /// Object enabled when at this distance zDistance from target
-    /// </summary>
-    private static float zEnableDistance =30f;
-    /// <summary>
-    /// Object enabled when at this distance from target
-    /// </summary>
-    private static float generalEnableDistance = 18;
-
-    /// <summary>
-    /// Culling target
-    /// </summary>
+    #region ============================================================================================== Public Fields
+    
     public static GameObject target { get; set; }
-    /// <summary>
-    /// Object used as owner of all culling coroutines
-    /// </summary>
+    
+    #endregion Public Fields
+    #region ============================================================================================= Private Fields
+    
+    private static float refreshCullingEverySeconds = 1f;
+    private static float zEnableDistance =30f;
+    private static float generalEnableDistance = 18;
     private static MyMonobehaviour coroutinesOwner;
-
-    //======================================== methods
+    
+    #endregion Private Fields
+    #region ============================================================================================= Public Methods
+    
     public void Init() {
         if (coroutinesOwner == null) {
             coroutinesOwner = new GameObject().AddComponent<MyMonobehaviour>();
             coroutinesOwner.gameObject.name = "Culling Coroutines Owner";
         }
-        coroutinesOwner.StartCoroutine(CullingCor());
+        coroutinesOwner.StartCoroutine(RefreshCullingCor());
     }
     
-    /// <summary>
-    /// Handles culling
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator CullingCor() {
+    #endregion Public Methods
+    #region ============================================================================================ Private Methods
+    
+    private IEnumerator RefreshCullingCor() {
         while (coroutinesOwner != null) {
             if((transform.position.z < target.transform.position.z && Vector3.Distance(transform.position, target.transform.position)<zEnableDistance)||
-                Vector3.Distance(transform.position, target.transform.position) < generalEnableDistance)
+               Vector3.Distance(transform.position, target.transform.position) < generalEnableDistance)
                 gameObject.SetActive(true);
             else
                 gameObject.SetActive(false);
-            yield return new WaitForSeconds(checkRateo);
+            yield return new WaitForSeconds(refreshCullingEverySeconds);
         }
     }
-
+    
     private void OnDestroy() {
         if (coroutinesOwner != null) {
             coroutinesOwner.StopAllCoroutines();
             Destroy(coroutinesOwner);
         }
     }
+    #endregion Private Methods
 }
