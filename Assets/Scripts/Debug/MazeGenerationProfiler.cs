503e31ebf1c4df34e5c15a2840e84982a6dab79c
nico-bertoli <nicolo.bertoli@miniclip.com>
2024-06-13 21:56:23 +0200

    maze generation profiler fix

--- Assets/Scripts/Debug/MazeGenerationProfiler.cs
+++ Assets/Scripts/Debug/MazeGenerationProfiler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class MazeGenerationProfiler : MonoBehaviour
 {
+    #if UNITY_EDITOR
     private float generationStartTime;
     private float generationEndTime;
     private void Start()
@@ -19,11 +20,12 @@ public class MazeGenerationProfiler : MonoBehaviour
     private void OnGenerationEnded()
     {
         generationEndTime = Time.time;
-        Debug.Log($"Generation took {Time.time - generationEndTime} seconds");
+        Debug.Log($"Generation took {Time.time - generationStartTime} seconds");
     }
 
     private void OnMazeChunksGenerated()
     {
         Debug.Log($"Maze mesh generation took {Time.time - generationEndTime} seconds");
     }
+    #endif
 }
