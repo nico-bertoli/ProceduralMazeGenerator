1d4b579717ab3e9d20509e8de5bd1706c4635062
nico-bertoli <nicobertoli.1999@gmail.com>
2024-06-16 16:46:39 +0200

    player movement improved

--- Assets/Scripts/GameObjects/Player.cs
+++ Assets/Scripts/GameObjects/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using Cinemachine.Utility;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,6 +11,8 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     [SerializeField] Rigidbody rigidBody;
+    
+    private Collision currentWallCollision;
 
     #endregion Private Fields
     #region ============================================================================================ Private Methods
@@ -17,14 +20,41 @@ public class Player : MonoBehaviour
     private void Update() => HandleMovement();
 
     private void HandleMovement() {
-        if (InputManager.Instance.IsMoving) {
+        if (InputManager.Instance.IsMoving)
+        {
             Vector3 moveDir = (-Vector3.forward * InputManager.Instance.MoveDirection.y - Vector3.right * InputManager.Instance.MoveDirection.x).normalized;
+            moveDir = RotateMoveDirectionAlongCollidingWall(moveDir);
             rigidBody.velocity = moveDir * moveSpeed;
         }
-        else {
+        else
+        {
             rigidBody.velocity = Vector3.zero;
         }
     }
+
+    private Vector3 RotateMoveDirectionAlongCollidingWall(Vector3 moveDir)
+    {
+        if (currentWallCollision == null)
+            return moveDir;
+        
+        Vector3 collisionNormal = currentWallCollision.contacts[0].normal;
+
+        if (Vector3.Angle(moveDir, collisionNormal) > 90)
+        {
+            Vector3 moveDirectionForcedByWall = moveDir.ProjectOntoPlane(collisionNormal).normalized;
+            // Debug.DrawLine(currentWallCollision.contacts[0].point,
+            //     currentWallCollision.contacts[0].point + moveDirectionForcedByWall * 100 - currentWallCollision.contacts[0].point,Color.red,
+            //     10);
+            return moveDirectionForcedByWall;
+        }
+        
+        Debug.LogError("angle was too large");
+        return moveDir;
+    }
     
+    private void OnCollisionStay(Collision collision) => currentWallCollision = collision;
+
+    private void OnCollisionExit(Collision other) => currentWallCollision = null;
+
     #endregion Private Methods
 }
