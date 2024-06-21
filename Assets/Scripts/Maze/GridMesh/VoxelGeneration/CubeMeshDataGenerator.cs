d7d7c2d3ed2d506ed2fec15a46cd11d21371e27f
nico-bertoli <nicobertoli.1999@gmail.com>
2024-06-22 00:01:04 +0200

    Added CubeMeshGenerator, Voxel generator refactored

---
+++ Assets/Scripts/Maze/GridMesh/VoxelGeneration/CubeMeshDataGenerator.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMeshDataGenerator : MonoBehaviour
+{
+    private enum CubeFaceDirection
+    {
+        Forward = 0,
+        Right = 1,
+        Back = 2,
+        Left = 3,
+        Up = 4,
+        Bottom = 5
+    }
+
+    private static readonly Vector3[] CubeVertices = new Vector3[]
+    {
+        new (1, 1,  1),
+        new (-1, 1, 1),
+        new (-1,-1, 1),
+        new (1, -1, 1),
+        new (-1, 1,-1),
+        new (1, 1, -1),
+        new (1, -1,-1),
+        new (-1, -1,-1),
+    };
+
+    private static readonly int[][] CubeTriangles = new int[][]
+    {
+        new [] { 0, 1, 2, 3 },
+        new [] { 5, 0, 3, 6 },
+        new [] { 4, 5, 6, 7 },
+        new [] { 1, 4, 7, 2 },
+        new [] { 5, 4, 1, 0 },
+        new [] { 3, 2, 7, 6 }
+    };
+    
+    public static void GetMeshData(List<Vector3> vertices, List<int> triangles, Vector3 scale, Vector3 position, bool createBottomFace = true)
+    {
+        foreach (CubeFaceDirection direction in Enum.GetValues(typeof(CubeFaceDirection)))
+        {
+            // prevents creating bottom face
+            if(createBottomFace == false && direction == CubeFaceDirection.Bottom)
+                continue;
+            
+            GetFaceData(vertices,triangles,direction, scale, position);
+        }
+    }
+
+    private static void GetFaceData(List<Vector3> vertices, List<int> triangles, CubeFaceDirection cubeFaceDirection, Vector3 scale, Vector3 position)
+    {
+        vertices.AddRange(GetFaceVertices(cubeFaceDirection,scale, position));
+        int vertCount = vertices.Count;
+        
+        triangles.Add(vertCount -4);
+        triangles.Add(vertCount -4 + 1);
+        triangles.Add(vertCount -4 + 2);
+        triangles.Add(vertCount -4);
+        triangles.Add(vertCount -4 + 2);
+        triangles.Add(vertCount -4 + 3);
+    }
+
+    private static Vector3[] GetFaceVertices(CubeFaceDirection faceDirection, Vector3 scale, Vector3 position)
+    {
+        Vector3[] faceVertices = new Vector3[4];
+        for (int i = 0; i < faceVertices.Length; i++)
+        {
+            var vertex = CubeVertices[CubeTriangles[(int)faceDirection][i]];
+            vertex.Scale(scale);
+            vertex += position;
+            faceVertices[i] = vertex;
+        }
+        return faceVertices;
+    }
+}
--- Assets/Scripts/Maze/GridMesh/VoxelGeneration/VoxelGenerator.cs
+++ Assets/Scripts/Maze/GridMesh/VoxelGeneration/VoxelGenerator.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class VoxelGenerator : MonoBehaviour
 {
+    public Action OnMeshGenerated;
+    
+    #region ============================================================================================= Private Fields
+
     [Header("References")] 
     [SerializeField] private VoxelChunk voxelChunkPrototype;
     [SerializeField] private MarginWallsGenerator marginWallsGenerator;
@@ -13,14 +17,15 @@ public class VoxelGenerator : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private Material material;
     [SerializeField] private float wallsHeight = 0.5f;
-
+    
     private List<VoxelChunk> chunks = new ();
-
+    
     private float wallsWidth => Settings.Instance.mazeGenerationSettings.IngameWallsWidth;
     private int chunkSize => Settings.Instance.mazeGenerationSettings.VoxelChunkSize;
-    
-    public Action OnMeshGenerated;
 
+    #endregion Private Fields
+    #region ============================================================================================ Pucblic Methods
+    
     public void CreateChunks(DataGrid dataGrid)
     {
         int mChunksCount = Mathf.CeilToInt(dataGrid.RowsCount / chunkSize) +1;
@@ -33,6 +38,9 @@ public class VoxelGenerator : MonoBehaviour
         marginWallsGenerator.InitMargins(dataGrid,wallsWidth);
         OnMeshGenerated?.Invoke();
     }
+    
+    #endregion Public Methods
+    #region ============================================================================================ Private Methods
 
     private VoxelChunk CreateChunk(DataGrid dataGrid, int mBegin, int nBegin, int chunkSize)
     {
@@ -54,12 +62,12 @@ public class VoxelGenerator : MonoBehaviour
                 if (cell.IsTopWallActive)
                 {
                     Vector3 topWallPos = new Vector3(cell.PosN - wallsOffsetFromCenter, 0, -cell.PosM);
-                    CreateCube(vertices,triangles,topWallScale * 0.5f,topWallPos);
+                    CubeMeshDataGenerator.GetMeshData(vertices,triangles,topWallScale * 0.5f,topWallPos,false);
                 }
                 if (cell.IsRightWallActive)
                 {
                     Vector3 rightWallPos = new Vector3(cell.PosN, 0, -cell.PosM - wallsOffsetFromCenter);
-                    CreateCube(vertices,triangles,rightWallScale * 0.5f,rightWallPos);
+                    CubeMeshDataGenerator.GetMeshData(vertices,triangles,rightWallScale * 0.5f,rightWallPos,false);
                 }
             }
         }
@@ -79,78 +87,7 @@ public class VoxelGenerator : MonoBehaviour
         
         marginWallsGenerator.Reset();
     }
-    
-    //-------------------------------------------------------------------------------
-
-    private void CreateCube(List<Vector3> vertices, List<int> triangles, Vector3 scale, Vector3 position)
-    {
-        foreach (CubeFaceDirection direction in Enum.GetValues(typeof(CubeFaceDirection)))
-        {
-            // prevents creating bottom face
-            if(direction == CubeFaceDirection.Bottom)
-                continue;
-            
-            CreateFace(vertices,triangles,direction, scale, position);
-        }
-    }
-
-    private void CreateFace(List<Vector3> vertices, List<int> triangles, CubeFaceDirection cubeFaceDirection, Vector3 scale, Vector3 position)
-    {
-        vertices.AddRange(GetFaceVertices(cubeFaceDirection,scale, position));
-        int vertCount = vertices.Count;
-        
-        triangles.Add(vertCount -4);
-        triangles.Add(vertCount -4 + 1);
-        triangles.Add(vertCount -4 + 2);
-        triangles.Add(vertCount -4);
-        triangles.Add(vertCount -4 + 2);
-        triangles.Add(vertCount -4 + 3);
-    }
-
-    private static Vector3[] GetFaceVertices(CubeFaceDirection faceDirection, Vector3 scale, Vector3 position)
-    {
-        Vector3[] faceVertices = new Vector3[4];
-        for (int i = 0; i < faceVertices.Length; i++)
-        {
-            var vertex = CubeVertices[CubeTriangles[(int)faceDirection][i]];
-            vertex.Scale(scale);
-            vertex += position;
-            faceVertices[i] = vertex;
-        }
-        return faceVertices;
-    }
-
-    //-------------------
-    
-    private enum CubeFaceDirection
-    {
-        Forward = 0,
-        Right = 1,
-        Back = 2,
-        Left = 3,
-        Up = 4,
-        Bottom = 5
-    }
-
-    private static readonly Vector3[] CubeVertices = new Vector3[]
-    {
-        new (1, 1,  1),
-        new (-1, 1, 1),
-        new (-1,-1, 1),
-        new (1, -1, 1),
-        new (-1, 1,-1),
-        new (1, 1, -1),
-        new (1, -1,-1),
-        new (-1, -1,-1),
-    };
-
-    private static readonly int[][] CubeTriangles = new int[][]
-    {
-        new [] { 0, 1, 2, 3 },
-        new [] { 5, 0, 3, 6 },
-        new [] { 4, 5, 6, 7 },
-        new [] { 1, 4, 7, 2 },
-        new [] { 5, 4, 1, 0 },
-        new [] { 3, 2, 7, 6 }
-    };
+    #endregion Private Methods
 }
+
+
