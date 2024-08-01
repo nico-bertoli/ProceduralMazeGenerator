efa44ef3aed9ac620d82d7ca78a48d129a63104d
nico-bertoli <nicobertoli.1999@gmail.com>
2024-08-01 16:02:14 +0200

    Update WallObject.cs

--- Assets/Scripts/Maze/GridMesh/LiveGridGeneration/WallObject.cs
+++ Assets/Scripts/Maze/GridMesh/LiveGridGeneration/WallObject.cs
@@ -6,10 +6,11 @@ public class WallObject : MonoBehaviour
     #region ============================================================================================= Private Fields
     [SerializeField] private MeshRenderer meshRenderer;
     private Vector3 localScale => transform.localScale;
-    
+
     #endregion Private Fields
-    #region ============================================================================================= Public Methods
-    
+    #region ============================================================================================= Methods
+
+    //-------------- public 
     public void SetWidth(float width) => transform.localScale = new Vector3(localScale.x, localScale.y,width);
     public void SetLength(float length) => transform.localScale = new Vector3(length , localScale.y, localScale.z);
     public void SetHeight(float height) => transform.localScale = new Vector3(localScale.x, height, localScale.z);
@@ -17,8 +18,12 @@ public class WallObject : MonoBehaviour
 
     public void SetMeshActive(bool _active) => meshRenderer.enabled = _active;
 
-    #endregion Public Methods
 
+    //-------------- private 
     private void Awake() => SetHeight(Settings.Instance.MazeGenerationSettings.WallsHeight);
+
+    #endregion Methods
+
+
 
 }
