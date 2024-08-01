ea2fed870d2f57c61c483ebb154056b0af50f379
nico-bertoli <nicobertoli.1999@gmail.com>
2024-08-01 16:01:18 +0200

    WallObject adjustments, prefabs sorted

--- Assets/Scripts/Maze/GridMesh/LiveGridGeneration/WallObject.cs
+++ Assets/Scripts/Maze/GridMesh/LiveGridGeneration/WallObject.cs
@@ -1,11 +1,10 @@
-using System;
 using UnityEngine;
 
+[RequireComponent (typeof(MeshRenderer))]
 public class WallObject : MonoBehaviour
 {
     #region ============================================================================================= Private Fields
-
-    private MeshRenderer meshRenderer;
+    [SerializeField] private MeshRenderer meshRenderer;
     private Vector3 localScale => transform.localScale;
     
     #endregion Private Fields
@@ -13,26 +12,13 @@ public class WallObject : MonoBehaviour
     
     public void SetWidth(float width) => transform.localScale = new Vector3(localScale.x, localScale.y,width);
     public void SetLength(float length) => transform.localScale = new Vector3(length , localScale.y, localScale.z);
-
     public void SetHeight(float height) => transform.localScale = new Vector3(localScale.x, height, localScale.z);
-
     public void SetPosition(float x, float z) => transform.position = new Vector3(x, 0, z);
 
-    public void SetMeshActive(bool _active)
-    {
-        if (meshRenderer == null)
-            meshRenderer = GetComponent<MeshRenderer>();
-        
-        meshRenderer.enabled = _active;
-    }
-    
+    public void SetMeshActive(bool _active) => meshRenderer.enabled = _active;
+
     #endregion Public Methods
-    #region ============================================================================================ Private Methods
 
-    private void Awake()
-    {
-        SetHeight(Settings.Instance.MazeGenerationSettings.WallsHeight);
-    }
+    private void Awake() => SetHeight(Settings.Instance.MazeGenerationSettings.WallsHeight);
 
-    #endregion Private Methods
 }
